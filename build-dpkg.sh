060a36dce927b284e77f6f325ba88eefca739491
Brian Boru <brian_boru@toavalon.com>
2018-07-25 23:25:51 +0300

    pkg

---
+++ build-dpkg.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p libex/usr/include/libex
+mkdir -p libex/usr/lib/pkgconfig
+mkdir -p libex/usr/share/doc/libex-dev
+cp libex.pc libex/usr/lib/pkgconfig
+cp .zbuild/_standard/_libs/libex.a libex/usr/lib
+cp include/libex/* libex/usr/include/libex
+cp doc/copyright libex/usr/share/doc/libex-dev
+cp doc/changelog libex/usr/share/doc/libex-dev/changelog.Debian
+gzip libex/usr/share/doc/libex-dev/changelog.Debian
+cd libex
+md5deep -r -l usr > DEBIAN/md5sums
+cd ..
+fakeroot dpkg-deb --build libex
+mv libex.deb libex-dev_`cat libex/DEBIAN/control | grep Version | awk '{print $2}'`_`cat libex/DEBIAN/control | grep Architecture | awk '{print $2}'`.deb
+rm -fr libex/usr/*
+rmdir libex/usr
+rm libex/DEBIAN/md5sums
